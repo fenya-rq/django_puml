41d82a847d4154298ca45a17f8a871defd7a113d
Yaroslav Panasenko <fenyochek@gmail.com>
2025-07-19 17:09:38 +0700

    fix: remove debug statements

--- src/plan/services/work_volume.py
+++ src/plan/services/work_volume.py
@@ -27,7 +27,6 @@ class WorkVolumeService:
         return False
 
     def create_or_update_work_volume(self):
-        print(self.data)
         return WorkVolume.objects.update_or_create(
             factory=self.factory,
             start=self.data['start'],
